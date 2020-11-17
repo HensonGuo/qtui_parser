2218cefb2d39125b9b9ee618308a842873f24742
郭昀霖 <1851228745@qq.com>
2020-11-17 09:56:52 +0800

    xml文件解析

---
+++ ui_parser.py
@@ -0,0 +1,118 @@
+#-*- coding:utf8 -*-
+
+from xml.etree.ElementTree import parse
+from PyQt4 import QtGui
+from ui_properties import UIProperties
+
+
+class UIParser(object):
+
+    def __init__(self, parent=None):
+        super(UIParser, self).__init__()
+        self._uiprops = UIProperties(self)
+        self._handerMap = {
+            "widget":self.createWidget,
+            "layout":self.createLayout,
+            "item": self.createItem,
+        }
+        self._debug = False
+        self._widget = None
+
+    def setDebug(self, bool):
+        self._debug = bool
+        self._uiprops.setDebug(bool)
+
+    def parse(self, uifile, res_prefix, parentWidget=None):
+        self._resPrefix = res_prefix
+        content = parse(uifile)
+        root = content.getroot()
+        self.everySubTrees(root, parentWidget)
+        return self._widget
+
+    def everySubTrees(self, widgetElement, parent):
+        for child in iter(widgetElement):
+            try:
+                handler = self._handerMap[child.tag]
+            except KeyError:
+                continue
+            handler(child, parent)
+
+    def createWidget(self, widgetElement, parent):
+        className = widgetElement.attrib["class"]
+        objName = self.getObjectName(widgetElement)
+        module = "QtGui.%s()" % className
+        widget = eval(module)
+        if not self._widget:
+            self._widget = widget
+        widget.setObjectName(objName)
+        if isinstance(parent, QtGui.QLayout):
+            parent.addWidget(widget)
+        else:
+            widget.setParent(parent)
+        self.printCreateOject(widget, module, parent)
+        self._uiprops.setProperties(widget, widgetElement)
+        self.everySubTrees(widgetElement, widget)
+        return widget
+
+    def createLayout(self, layoutElement, parent):
+        className = layoutElement.attrib["class"]
+        objName = self.getObjectName(layoutElement)
+        module = "QtGui.%s()" % className
+        layout = eval(module)
+        layout.setObjectName(objName)
+        if isinstance(parent, QtGui.QLayout):
+            parent.addLayout(layout)
+        else:
+            parent.setLayout(layout)
+        self.printCreateOject(layout, module, parent)
+        if parent.objectName() == "widgetWarning":
+            print 'aa'
+
+        left = self._uiprops.getProperty(layoutElement, 'leftMargin', -1)
+        top = self._uiprops.getProperty(layoutElement, 'topMargin', -1)
+        right = self._uiprops.getProperty(layoutElement, 'rightMargin', -1)
+        bottom = self._uiprops.getProperty(layoutElement, 'bottomMargin', -1)
+        layout.setContentsMargins(left, top, right, bottom)
+
+        self._uiprops.setProperties(layout, layoutElement)
+        self.everySubTrees(layoutElement, layout)
+        return layout
+
+    def createItem(self, itemElement, parent):
+        if isinstance(parent, QtGui.QLayout):
+            self.everySubTrees(itemElement, parent)
+
+    def getObjectName(self, element):
+        className = element.attrib["class"]
+        objectName = element.attrib["name"]
+        if not objectName:
+            objectName = className[0].lower() + className[1:]
+        return objectName
+
+    def printCreateOject(self, obj, createModule, parent):
+        if self._debug:
+            print "\n%s = %s" % (obj.objectName(), createModule)
+            if not parent:
+                return
+            if isinstance(parent, QtGui.QLayout):
+                if isinstance(obj, QtGui.QLayout):
+                    print "%s.addLayout(%s)" % (parent.objectName(), obj.objectName())
+                else:
+                    print "%s.addWidget(%s)" % (parent.objectName(), obj.objectName())
+            else:
+                if isinstance(obj, QtGui.QLayout):
+                    print "%s.setLayout(%s)" % (obj.objectName(), parent.objectName())
+                else:
+                    print "%s.setParent(%s)" % (obj.objectName(), parent.objectName())
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    uiparser = UIParser()
+    uiparser.setDebug(True)
+    widget = uiparser.parse(r"D:\Work\apps_wonderful\transformer\gamelive\ent_vote\entertainment_vote\ui\historyform.ui", None)
+    widget.show()
+    app.exec_()
---
+++ ui_properties.py
@@ -0,0 +1,104 @@
+#-*- coding:utf8 -*-
+from PyQt4 import QtGui, QtCore
+
+
+class UIProperties(object):
+    # ignore the propertys
+    Ignores = ["leftMargin", "topMargin", "rightMargin", "bottomMargin"]
+
+    def __init__(self, parent):
+        self._debug = False
+
+    def setDebug(self, bool):
+        self._debug = bool
+
+    def setProperties(self, widget, elem):
+        for prop in elem.findall("property"):
+            prop_name = prop.attrib["name"]
+            if prop_name in UIProperties.Ignores:
+                continue
+            func_name = self.getPropertySetterName(prop_name)
+            func_value = self.getPropertySetterValue(prop)
+            func = getattr(widget, func_name)
+            if func:
+                func(func_value)
+                if self._debug:
+                    print u"{}.{}({})".format(widget.objectName(), func_name, func_value)
+            else:
+                # todo 不支持的后续再做支持
+                print "not support %s" % func_name
+
+    def getProperty(self, elem, name, default=None):
+        return self._getChild("property", elem, name, default)
+
+    def _getChild(self, elem_tag, elem, name, default=None):
+        for prop in elem.findall(elem_tag):
+            if prop.attrib["name"] == name:
+                return self.getPropertySetterValue(prop)
+        else:
+            return default
+
+    def getPropertySetterName(self, prop_name):
+        return "set%s%s" % (prop_name[0].upper(), prop_name[1:])
+
+    def getPropertySetterValue(self, prop):
+        valueElement = prop[0]
+        tag = valueElement.tag
+        func_name = self.getPropertyGetterName(tag)
+        func = getattr(self, func_name)
+        if func:
+            return func(valueElement)
+        else:
+            # todo 不支持的后续再做支持
+            print "not support %s" % func_name
+
+    def getPropertyGetterName(self, tag):
+        return "get%s%s" % (tag[0].upper(), tag[1:])
+
+    def getString(self, prop, notr=None):
+        text = prop.text
+
+        if text is None:
+            return ""
+
+        if prop.get('notr', notr) == 'true':
+            return text
+
+        disambig = prop.get('comment')
+        encoding = QtGui.QApplication.UnicodeUTF8
+        translated = QtGui.QApplication.translate("", text,
+                disambig, encoding)
+        return translated
+
+    def getNumber(self, prop):
+        return int(prop.text)
+
+    def getDouble(self, prop):
+        return float(prop.text)
+
+    def getBool(self, prop):
+        return prop.text == 'true'
+
+    def getSize(self, prop):
+        return QtCore.QSize(*int_list(prop))
+
+    def getSizef(self, prop):
+        return QtCore.QSize(*float_list(prop))
+
+    def getPoint(self, prop):
+        return QtCore.QPoint(*int_list(prop))
+
+    def getPointf(self, prop):
+        return QtCore.QPointF(*float_list(prop))
+
+    def getRect(self, prop):
+        return QtCore.QRect(*int_list(prop))
+
+    def getRectf(self, prop):
+        return QtCore.QRectF(*float_list(prop))
+
+def int_list(prop):
+    return [int(child.text) for child in prop]
+
+def float_list(prop):
+    return [float(child.text) for child in prop]
